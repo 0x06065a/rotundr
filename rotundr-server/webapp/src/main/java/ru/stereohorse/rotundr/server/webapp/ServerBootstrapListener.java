4dbc18b6e8aeef5458f776f07e28f5ac4d6ab514
alien <0x06065a@gmail.com>
2013-09-08 21:39:00 +0400

    moved intercom to separate dir

--- rotundr-server/webapp/src/main/java/ru/stereohorse/rotundr/server/webapp/ServerBootstrapListener.java
+++ rotundr-server/webapp/src/main/java/ru/stereohorse/rotundr/server/webapp/ServerBootstrapListener.java
@@ -2,7 +2,7 @@ package ru.stereohorse.rotundr.server.webapp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.stereohorse.rotundr.server.core.Server;
+import ru.stereohorse.rotundr.network.Server;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
